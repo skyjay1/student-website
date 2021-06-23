ec477fb7bb3470b06e52aa18be1dd85519ff7c0c
Skyler James <15920401+skyjay1@users.noreply.github.com>
2021-06-23 17:33:43 -0600

    Base website

---
+++ js/footer.js
@@ -0,0 +1,9 @@
+// insert the navigation bar in the header
+document.write(`
+<footer>
+  <hr>
+  <a href="https://icarus.cs.weber.edu/~sj57573/"><img class="logo" src="img/skyjay1-logo1-round-alpha.png" alt="skyjay1 logo"></a>
+  <br>
+  &copy; 2020
+</footer>
+`)
---
+++ js/header.js
@@ -0,0 +1,12 @@
+// insert the navigation bar in the header
+document.write(`
+<header>
+  <nav>
+    <a href="index.html">Profile</a>
+    <a href="academics.html">Academics</a>
+    <a href="art.html">Artwork</a>
+    <a href="coding.html">Coding</a>
+    <a href="classes.html">CS Courses</a>
+  </nav>
+</header>
+`)
